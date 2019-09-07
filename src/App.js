9409e623063a38d035117e887eab6769dd81e24a
bvasilop <bvasilop@gmail.com>
2019-09-06 18:53:57 -0700

    added project files for React Clock

---
+++ src/App.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import './App.css'
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      time: new Date().toLocaleString()
+    };
+  }
+  componentDidMount() {
+    this.intervalID = setInterval(
+      () => this.tick(),
+      1000
+    );
+  }
+  componentWillUnmount() {
+    clearInterval(this.intervalID);
+  }
+  tick() {
+    this.setState({
+      time: new Date().toLocaleString()
+    });
+  }
+  render() {
+    const { time } = this.state;
+    return (
+      <div className="App-clock">
+      <h1> Hello, Seattle</h1>
+      <h2>it is currently {time}</h2>
+      </div>
+
+    );
+  }
+}
+
+
+export default App;
